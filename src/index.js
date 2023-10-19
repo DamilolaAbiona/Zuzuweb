5b1a474fba53948f04506a0c10bb96f145334e6d
DamilolaAbiona <99470227+DamilolaAbiona@users.noreply.github.com>
2023-10-19 21:03:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 root.render(
   <React.StrictMode>
     <BrowserRouter>
